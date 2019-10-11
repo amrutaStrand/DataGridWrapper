dc37af65059298b18d6679af1df9cdc39c0a81e4
Shivam Santhosh Bhosale <shivam.bhosale@strandls.com>
2019-10-11 14:08:35 +0530

    merging s5 changes to trunk.

---
+++ dataset/com/strandgenomics/cube/dataset/BasicColumnOperations.cs
@@ -0,0 +1,539 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com.strandgenomics.cube.dataset
+{
+    /// <summary>
+    /// Basic column operations like column + column or column + value etc.
+    /// <br>
+    /// NOTE: THIS CODE IS GENERATED FROM BasicColumnOperations.py. *** DO NOT EDIT ***
+    /// </summary>
+    public class BasicColumnOperations: ColumnOperationUtil
+    {
+        public static IColumn Add(IColumn a, IColumn b, string name)
+        {
+            if (name == null)
+                name = a.GetName() + " + " + b.GetName();
+
+            if (!IsNumericColumn(a) || !IsNumericColumn(b))
+                throw new Exception(
+                "+ operation can be performed only on int and float columns.");
+
+            if (DatasetUtil.IsFloatColumn(a) || DatasetUtil.IsFloatColumn(b))
+            {
+                float[] data = Add_float(a, b, name);
+                return CreateFloatColumn(name, data, a, b);
+            }
+
+            if (DatasetUtil.IsIntegerColumn(a) && DatasetUtil.IsIntegerColumn(b))
+            {
+                int[] data = Add_int(a, b, name);
+                return CreateIntColumn(name, data, a, b);
+            }
+            else
+                throw new Exception("huh?");
+        }
+
+        public static IColumn Add(IColumn a, object b, string name)
+        {
+            if (name == null)
+                name = a.GetName() + " + " + b.ToString();
+
+            if (!IsNumericColumn(a))
+                throw new Exception(
+                "+ operation can be performed only on int and float columns.");
+
+            if (!((b is int)|| (b is float) || (b is double)))
+                throw new Exception(
+                "+ operation can be performed only with numbers.");
+
+            if (DatasetUtil.IsFloatColumn(a) || b is float || b is double)
+            {
+                float[] data = Add_float(a, ToFloat(b), name);
+                return CreateFloatColumn(name, data, a);
+            }
+
+            if (DatasetUtil.IsIntegerColumn(a) && b is int)
+            {
+                int[] data = Add_int(a, ToInt(b), name);
+                return CreateIntColumn(name, data, a);
+            }
+
+            else
+                throw new Exception("huh?");
+        }
+
+        private static float[] Add_float(IColumn a, IColumn b, string name)
+        {
+
+            float[] data = new float[a.GetSize()];
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                float x = a.GetFloat(i);
+                float y = b.GetFloat(i);
+
+                if (x == FLOAT_MV || y == FLOAT_MV)
+                    data[i] = FLOAT_MV;
+                else
+                    data[i] = x + y;
+            }
+
+            return data;
+        }
+
+        private static float[] Add_float(IColumn a, float b, string name)
+        {
+
+            float[] data = new float[a.GetSize()];
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                float x = a.GetFloat(i);
+                float y = b;
+
+                if (x == FLOAT_MV || y == FLOAT_MV)
+                    data[i] = FLOAT_MV;
+                else
+                    data[i] = x + y;
+            }
+
+            return data;
+        }
+
+        private static int[] Add_int(IColumn a, IColumn b, string name)
+        {
+
+            int[] data = new int[a.GetSize()];
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                int x = a.GetInt(i);
+                int y = b.GetInt(i);
+
+                if (x == INT_MV || y == INT_MV)
+                    data[i] = INT_MV;
+                else
+                    data[i] = x + y;
+            }
+
+            return data;
+        }
+
+        private static int[] Add_int(IColumn a, int b, string name)
+        {
+
+            int[] data = new int[a.GetSize()];
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                int x = a.GetInt(i);
+                int y = b;
+
+                if (x == INT_MV || y == INT_MV)
+                    data[i] = INT_MV;
+                else
+                    data[i] = x + y;
+            }
+
+            return data;
+        }
+
+        public static IColumn Sub(IColumn a, IColumn b, string name)
+        {
+            if (name == null)
+                name = a.GetName() + " - " + b.GetName();
+
+            if (!IsNumericColumn(a) || !IsNumericColumn(b))
+                throw new Exception(
+                "- operation can be performed only on int and float columns.");
+
+            if (DatasetUtil.IsFloatColumn(a) || DatasetUtil.IsFloatColumn(b))
+            {
+                float[] data = Sub_float(a, b, name);
+                return CreateFloatColumn(name, data, a, b);
+            }
+
+            if (DatasetUtil.IsIntegerColumn(a) && DatasetUtil.IsIntegerColumn(b))
+            {
+                int[] data = Sub_int(a, b, name);
+                return CreateIntColumn(name, data, a, b);
+            }
+            else
+                throw new Exception("huh?");
+        }
+
+        public static IColumn Sub(IColumn a, object b, string name)
+        {
+            if (name == null)
+                name = a.GetName() + " - " + b.ToString();
+
+            if (!IsNumericColumn(a))
+                throw new Exception(
+                "- operation can be performed only on int and float columns.");
+
+            if (!((b is int) || (b is float) || (b is double)))
+                throw new Exception(
+                "- operation can be performed only with numbers.");
+
+            if (DatasetUtil.IsFloatColumn(a) || b is float || b is double)
+            {
+                float[] data = Sub_float(a, ToFloat(b), name);
+                return CreateFloatColumn(name, data, a);
+            }
+
+            if (DatasetUtil.IsIntegerColumn(a) && b is int)
+            {
+                int[] data = Sub_int(a, ToInt(b), name);
+                return CreateIntColumn(name, data, a);
+            }
+
+            else
+                throw new Exception("huh?");
+        }
+
+        private static float[] Sub_float(IColumn a, IColumn b, string name)
+        {
+
+            float[] data = new float[a.GetSize()];
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                float x = a.GetFloat(i);
+                float y = b.GetFloat(i);
+
+                if (x == FLOAT_MV || y == FLOAT_MV)
+                    data[i] = FLOAT_MV;
+                else
+                    data[i] = x - y;
+            }
+
+            return data;
+        }
+        private static float[] Sub_float(IColumn a, float b, String name)
+        {
+
+            float[] data = new float[a.GetSize()];
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                float x = a.GetFloat(i);
+                float y = b;
+
+                if (x == FLOAT_MV || y == FLOAT_MV)
+                    data[i] = FLOAT_MV;
+                else
+                    data[i] = x - y;
+            }
+
+            return data;
+        }
+
+
+        private static int[] Sub_int(IColumn a, IColumn b, string name)
+        {
+
+            int[] data = new int[a.GetSize()];
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                int x = a.GetInt(i);
+                int y = b.GetInt(i);
+
+                if (x == INT_MV || y == INT_MV)
+                    data[i] = INT_MV;
+                else
+                    data[i] = x - y;
+            }
+
+            return data;
+        }
+
+        private static int[] Sub_int(IColumn a, int b, string name)
+        {
+
+            int[] data = new int[a.GetSize()];
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                int x = a.GetInt(i);
+                int y = b;
+
+                if (x == INT_MV || y == INT_MV)
+                    data[i] = INT_MV;
+                else
+                    data[i] = x - y;
+            }
+
+            return data;
+        }
+
+        public static IColumn Mul(IColumn a, IColumn b, string name)
+        {
+            if (name == null)
+                name = a.GetName() + " * " + b.GetName();
+
+            if (!IsNumericColumn(a) || !IsNumericColumn(b))
+                throw new Exception(
+                "* operation can be performed only on int and float columns.");
+
+            if (DatasetUtil.IsFloatColumn(a) || DatasetUtil.IsFloatColumn(b))
+            {
+                float[] data = Mul_float(a, b, name);
+                return CreateFloatColumn(name, data, a, b);
+            }
+
+            if (DatasetUtil.IsIntegerColumn(a) && DatasetUtil.IsIntegerColumn(b))
+            {
+                int[] data = Mul_int(a, b, name);
+                return CreateIntColumn(name, data, a, b);
+            }
+            else
+                throw new Exception("huh?");
+        }
+
+
+        public static IColumn Mul(IColumn a, object b, string name)
+        {
+            if (name == null)
+                name = a.GetName() + " * " + b.ToString();
+
+            if (!IsNumericColumn(a))
+                throw new Exception(
+                "* operation can be performed only on int and float columns.");
+
+            if (!((b is int) || (b is float) || (b is double)))
+                throw new Exception(
+                "* operation can be performed only with numbers.");
+
+            if (DatasetUtil.IsFloatColumn(a) || b is float || b is double)
+            {
+                float[] data = Mul_float(a, ToFloat(b), name);
+                return CreateFloatColumn(name, data, a);
+            }
+
+            if (DatasetUtil.IsIntegerColumn(a) && b is int)
+            {
+                int[] data = Mul_int(a, ToInt(b), name);
+                return CreateIntColumn(name, data, a);
+            }
+
+            else
+                throw new Exception("huh?");
+        }
+
+        private static float[] Mul_float(IColumn a, IColumn b, string name)
+        {
+
+            float[] data = new float[a.GetSize()];
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                float x = a.GetFloat(i);
+                float y = b.GetFloat(i);
+
+                if (x == FLOAT_MV || y == FLOAT_MV)
+                    data[i] = FLOAT_MV;
+                else
+                    data[i] = x * y;
+            }
+
+            return data;
+        }
+
+        private static float[] Mul_float(IColumn a, float b, string name)
+        {
+
+            float[] data = new float[a.GetSize()];
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                float x = a.GetFloat(i);
+                float y = b;
+
+                if (x == FLOAT_MV || y == FLOAT_MV)
+                    data[i] = FLOAT_MV;
+                else
+                    data[i] = x * y;
+            }
+
+            return data;
+        }
+
+
+        private static int[] Mul_int(IColumn a, IColumn b, string name)
+        {
+
+            int[] data = new int[a.GetSize()];
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                int x = a.GetInt(i);
+                int y = b.GetInt(i);
+
+                if (x == INT_MV || y == INT_MV)
+                    data[i] = INT_MV;
+                else
+                    data[i] = x * y;
+            }
+
+            return data;
+        }
+
+        private static int[] Mul_int(IColumn a, int b, string name)
+        {
+
+            int[] data = new int[a.GetSize()];
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                int x = a.GetInt(i);
+                int y = b;
+
+                if (x == INT_MV || y == INT_MV)
+                    data[i] = INT_MV;
+                else
+                    data[i] = x * y;
+            }
+
+            return data;
+        }
+
+
+        public static IColumn Div(IColumn a, IColumn b, string name)
+        {
+            if (name == null)
+                name = a.GetName() + " / " + b.GetName();
+
+            if (!IsNumericColumn(a) || !IsNumericColumn(b))
+                throw new Exception(
+                "/ operation can be performed only on int and float columns.");
+
+            if (DatasetUtil.IsFloatColumn(a) || DatasetUtil.IsFloatColumn(b))
+            {
+                float[] data = Div_float(a, b, name);
+                return CreateFloatColumn(name, data, a, b);
+            }
+
+            if (DatasetUtil.IsIntegerColumn(a) && DatasetUtil.IsIntegerColumn(b))
+            {
+                int[] data = Div_int(a, b, name);
+                return CreateIntColumn(name, data, a, b);
+            }
+            else
+                throw new Exception("huh?");
+        }
+
+        public static IColumn Div(IColumn a, object b, string name)
+        {
+            if (name == null)
+                name = a.GetName() + " / " + b.ToString();
+
+            if (!IsNumericColumn(a))
+                throw new Exception(
+                "/ operation can be performed only on int and float columns.");
+
+            if (!((b is int) || (b is float) || (b is double)))
+                throw new Exception(
+                "/ operation can be performed only with numbers.");
+
+            if (DatasetUtil.IsFloatColumn(a) || b is float || b is double)
+            {
+                float[] data = Div_float(a, ToFloat(b), name);
+                return CreateFloatColumn(name, data, a);
+            }
+
+            if (DatasetUtil.IsIntegerColumn(a) && b is int)
+            {
+                int[] data = Div_int(a, ToInt(b), name);
+                return CreateIntColumn(name, data, a);
+            }
+
+            else
+                throw new Exception("huh?");
+        }
+
+        private static float[] Div_float(IColumn a, IColumn b, string name)
+        {
+
+            float[] data = new float[a.GetSize()];
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                float x = a.GetFloat(i);
+                float y = b.GetFloat(i);
+
+                if (x == FLOAT_MV || y == FLOAT_MV || y == 0)
+                    data[i] = FLOAT_MV;
+                else
+                    data[i] = x / y;
+            }
+
+            return data;
+        }
+
+
+        private static float[] Div_float(IColumn a, float b, string name)
+        {
+
+            float[] data = new float[a.GetSize()];
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                float x = a.GetFloat(i);
+                float y = b;
+
+                if (x == FLOAT_MV || y == FLOAT_MV || y == 0)
+                    data[i] = FLOAT_MV;
+                else
+                    data[i] = x / y;
+            }
+
+            return data;
+        }
+
+        private static int[] Div_int(IColumn a, IColumn b, string name)
+        {
+
+            int[] data = new int[a.GetSize()];
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                int x = a.GetInt(i);
+                int y = b.GetInt(i);
+
+                if (x == INT_MV || y == INT_MV || y == 0)
+                    data[i] = INT_MV;
+                else
+                    data[i] = x / y;
+            }
+
+            return data;
+        }
+
+        private static int[] Div_int(IColumn a, int b, string name)
+        {
+
+            int[] data = new int[a.GetSize()];
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                int x = a.GetInt(i);
+                int y = b;
+
+                if (x == INT_MV || y == INT_MV || y == 0)
+                    data[i] = INT_MV;
+                else
+                    data[i] = x / y;
+            }
+
+            return data;
+        }
+
+
+
+    }
+}
--- dataset/com/strandgenomics/cube/dataset/ColumnOperationUtil.cs
+++ dataset/com/strandgenomics/cube/dataset/ColumnOperationUtil.cs
@@ -1,15 +1,16 @@
 ﻿using System;
+using System.Collections.Generic;
 namespace com.strandgenomics.cube.dataset
 
 {
     public class ColumnOperationUtil 
     {
-        protected const float FLOAT_MV = DatasetConstants.FLOAT_MV;
-        protected const int INT_MV = DatasetConstants.INTEGER_MV;
+        protected static float FLOAT_MV = DatasetConstants.FLOAT_MV;
+        protected static int INT_MV = DatasetConstants.INTEGER_MV;
         
         protected static bool IsNumericColumn(IColumn c)
         {
-            return DatasetUtil.IsFloatColumn(c) || DatasetUtil.isIntegerColumn(c);
+            return DatasetUtil.IsFloatColumn(c) || DatasetUtil.IsIntegerColumn(c);
         }
 
         protected static float ToFloat(Object o)
@@ -22,140 +23,123 @@ namespace com.strandgenomics.cube.dataset
             return (int) o;
         }
 
-        internal static void UpdateMetaData(IColumn clone, IColumn column)
+        /// <summary>
+        /// Creates a float column with the specified name and data.
+        /// mark and groupInfo of the source are propagated to the new column.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="data"></param>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        protected static IColumn CreateFloatColumn(string name, float[] data, IColumn source)
         {
-            throw new NotImplementedException();
+            IColumn c = ColumnFactory.CreateFloatColumn(name, data);
+
+            UpdateMetaData(c, source);
+            return c;
+        }
+
+        /// <summary>
+        /// Creates a float column with the specified name and data.
+        /// mark and groupInfo of the source are propagated to the new column
+        /// if both the sources have mark/groupInfo.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="data"></param>
+        /// <param name="source1"></param>
+        /// <param name="source2"></param>
+        /// <returns></returns>
+        protected static IColumn CreateFloatColumn(
+        string name, float[] data,
+        IColumn source1, IColumn source2)
+        {
+
+            IColumn c = ColumnFactory.CreateFloatColumn(name, data);
+            UpdateMetaData(c, source1, source2);
+            return c;
+        }
+
+        /// <summary>
+        /// Creates a int column with the specified name and data.
+        /// mark and groupInfo of the source are propagated to the new column.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="data"></param>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        protected static IColumn CreateIntColumn(string name, int[] data, IColumn source)
+        {
+            IColumn c = ColumnFactory.CreateIntColumn(name, data);
+
+            UpdateMetaData(c, source);
+            return c;
+        }
+
+        /// <summary>
+        /// Creates a int column with the specified name and data.
+        /// mark and groupInfo of the source are propagated to the new column
+        /// if both the sources have mark/groupInfo.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="data"></param>
+        /// <param name="source1"></param>
+        /// <param name="source2"></param>
+        /// <returns></returns>
+        protected static IColumn CreateIntColumn(
+        string name, int[] data,
+        IColumn source1, IColumn source2)
+        {
+
+            IColumn c = ColumnFactory.CreateIntColumn(name, data);
+            UpdateMetaData(c, source1, source2);
+            return c;
         }
 
 
-        //protected static IColumn createFloatColumn(String name, float[] data, IColumn source)
-        //{
-        //    IColumn c = ColumnFactory.createFloatColumn(name, data);
+        public static void UpdateMetaData(IColumn result, IColumn source)
+        {
+            ColumnMetaData p = source.GetMetaData();
+            ColumnMetaData x = result.GetMetaData();
 
-        //    updateMetaData(c, source);
-        //    return c;
-        //}
+            x.SetSource(p.GetSource());
+            x.SetMark(p.GetMark());
+
+            Dictionary<object,object> ms = p.GetState();
+            Dictionary<object, object> mr = x.GetState();
+            foreach (var item in ms)
+            {
+                mr.Add(item.Key, item.Value);
+            }
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="result">result column</param>
+        /// <param name="source1">first source column</param>
+        /// <param name="source2">second source column</param>
+        public static void UpdateMetaData(IColumn result, IColumn source1, IColumn source2)
+        {
+
+            ColumnMetaData p = source1.GetMetaData();
+            ColumnMetaData q = source2.GetMetaData();
+
+            ColumnMetaData x = result.GetMetaData();
+
+            if (p.GetSource() != null && p.GetSource().Equals(q.GetSource()))
+                x.SetSource(p.GetSource());
+
+            if (p.GetMark() != null && p.GetMark().Equals(q.GetMark()))
+                x.SetMark(p.GetMark());
+        }
+
+        private static bool SafeEquals(object a, object b)
+        {
+            return (a == null) ? (b == null) : a.Equals(b);
+        }
+
+
+
     }
 }
-
-//public class ColumnOperationUtil
-//{
-
-//    protected static final float FLOAT_MV = IColumn.FLOAT_MV;
-//    protected static final int INT_MV = IColumn.INTEGER_MV;
-
-//    protected static boolean isNumericColumn(IColumn c)
-//    {
-//        return DatasetUtil.isFloatColumn(c) || DatasetUtil.isIntegerColumn(c);
-//    }
-
-//    protected static float toFloat(Object o)
-//    {
-//        return ((Number)o).floatValue();
-//    }
-
-//    protected static int toInt(Object o)
-//    {
-//        return ((Number)o).intValue();
-//    }
-
-
-//    // {{{ createXXXColumn 
-
-//    /**
-//     * Creates a float column with the specified name and data.
-//     * mark and groupInfo of the source are propagated to the new column.
-//     */
-//    protected static IColumn createFloatColumn(String name, float[] data, IColumn source)
-//    {
-//        IColumn c = ColumnFactory.createFloatColumn(name, data);
-
-//        updateMetaData(c, source);
-//        return c;
-//    }
-
-//    /**
-//     * Creates a float column with the specified name and data.
-//     * mark and groupInfo of the source are propagated to the new column
-//     * if both the sources have mark/groupInfo.
-//     */
-//    protected static IColumn createFloatColumn(
-//        String name, float[] data,
-//        IColumn source1, IColumn source2)
-//    {
-
-//        IColumn c = ColumnFactory.createFloatColumn(name, data);
-//        updateMetaData(c, source1, source2);
-//        return c;
-//    }
-
-//    /**
-//     * Creates a int column with the specified name and data.
-//     * mark and groupInfo of the source are propagated to the new column.
-//     */
-//    protected static IColumn createIntColumn(String name, int[] data, IColumn source)
-//    {
-//        IColumn c = ColumnFactory.createIntColumn(name, data);
-
-//        updateMetaData(c, source);
-//        return c;
-//    }
-
-//    /**
-//     * Creates a int column with the specified name and data.
-//     * mark and groupInfo of the source are propagated to the new column
-//     * if both the sources have mark/groupInfo.
-//     */
-//    protected static IColumn createIntColumn(
-//        String name, int[] data,
-//        IColumn source1, IColumn source2)
-//    {
-
-//        IColumn c = ColumnFactory.createIntColumn(name, data);
-//        updateMetaData(c, source1, source2);
-//        return c;
-//    }
-
-//    // }}}
-
-//    // {{{ updateMetaData
-//    public static void updateMetaData(IColumn result, IColumn source)
-//    {
-//        ColumnMetaData p = source.getMetaData();
-//        ColumnMetaData x = result.getMetaData();
-
-//        x.setSource(p.getSource());
-//        x.setMark(p.getMark());
-
-//        Map ms = p.getState();
-//        Map mr = x.getState();
-//        mr.putAll(ms);
-//    }
-
-//    /**
-//     * @param result result column
-//     * @param a		first source column
-//     * @param b		second source column
-//     */
-//    public static void updateMetaData(IColumn result, IColumn source1, IColumn source2)
-//    {
-
-//        ColumnMetaData p = source1.getMetaData();
-//        ColumnMetaData q = source2.getMetaData();
-
-//        ColumnMetaData x = result.getMetaData();
-
-//        if (p.getSource() != null && p.getSource().equals(q.getSource()))
-//            x.setSource(p.getSource());
-
-//        if (p.getMark() != null && p.getMark().equals(q.getMark()))
-//            x.setMark(p.getMark());
-//    }
-
-//    private static boolean safeEquals(Object a, Object b)
-//    {
-//        return (a == null) ? (b == null) : a.equals(b);
-//    }
-//    // }}}
-//}
-
